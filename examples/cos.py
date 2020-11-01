cac5d4129e42ecc159bcaf7ca857b7cc9aa9ddf9
Laurent Martin <laurent.martin.l@gmail.com>
2020-11-01 23:40:58 +0100

    show differences

--- examples/cos.py
+++ examples/cos.py
@@ -8,23 +8,41 @@ import logging
 import json
 import sys
 
+# get file to upload from command line
+files_to_upload = sys.argv
+destination_folder="/"
+
+# get bucket information from config file
 cos_conf=setup.CONFIG['cos']
+
+# get Aspera Transfer Service Node information for specified COS bucket
 node_info=faspmanager_cos.node_info(cos_conf['service_credential_file'],cos_conf['bucket_region'],cos_conf['bucket_name'])
 
-upload_files = sys.argv
+# prepare node API request for upload_setup
+upload_setup_request = {"transfer_requests":[{"transfer_request":{"paths":[{"destination":destination_folder}]}}]}
 
-# A- call node API with one transfer request to get one transfer spec
-upload_setup_data = {"transfer_requests":[{"transfer_request":{"paths":[{"destination":"/"}]}}]}
-response = requests.post(node_info['url'] + '/files/upload_setup', data=json.dumps(upload_setup_data), auth=node_info['auth'], headers=node_info['headers'])
+# call Node API with one transfer request to get one transfer spec
+response = requests.post(
+    node_info['url'] + '/files/upload_setup', 
+    data=json.dumps(upload_setup_request), 
+    auth=node_info['auth'], 
+    headers=node_info['headers'])
 if response.status_code != 200:
     raise Exception("error")
-# extract the single transfer spec for a single transfer request
-tspec = response.json()['transfer_specs'][0]['transfer_spec']
+
+response_data = response.json()
+
+# extract the single transfer spec (we sent a single transfer request)
+t_spec = response_data['transfer_specs'][0]['transfer_spec']
+
 # add COS specific authorization info
-tspec.update(node_info['tspec'])
-logging.debug(tspec)
-# set file to upload, as usual
-tspec['paths'] = [{'source':'/Users/laurent/Documents/Samples/200KB.1'}]
-tspec['create_dir'] = True
-# B- start transfer, here we use the FASP Manager, but the newer Transfer SDK can be used as well
-faspmanager_helper.start_transfer_and_wait(tspec)
+t_spec.update(node_info['t_spec'])
+logging.debug(t_spec)
+
+# add file list in transfer spec
+t_spec['paths'] = []
+for f in files_to_upload:
+    t_spec['paths'].append({'source':f})
+
+# start transfer, here we use the FASP Manager, but the newer Transfer SDK can be used as well
+faspmanager_helper.start_transfer_and_wait(t_spec)
--- examples/node.py
+++ examples/node.py
@@ -3,43 +3,40 @@ import setup
 import faspmanager
 import faspmanager_helper
 import requests
-import requests.auth
 import logging
+import requests.auth
 import json
 import sys
 
-# files to send
-upload_files = sys.argv
+# get file to upload from command line
+files_to_upload = sys.argv
+destination_folder="/Upload"
 
-# package creation information
-upload_request = {
-    "transfer_requests":[
-        {"transfer_request":{"paths":[{"destination":"/Upload"}]}}]}
-logging.debug("req=%s",upload_request)
+# prepare node API request for upload_setup
+upload_setup_request = {"transfer_requests":[{"transfer_request":{"paths":[{"destination":destination_folder}]}}]}
 
-# create package and get information for file upload
+# call Node API with one transfer request to get one transfer spec
 response = requests.post(
     setup.CONFIG['node']['url'] + '/files/upload_setup',
     auth=requests.auth.HTTPBasicAuth(setup.CONFIG['node']['user'], setup.CONFIG['node']['pass']),
-    data=json.dumps(upload_request),
+    data=json.dumps(upload_setup_request),
     headers={
         "Content-Type": "application/json",
         "Accept": "application/json"
     },
     verify=False)
+if response.status_code != 200:
+    raise Exception("error")
+
 response_data = response.json()
 
-logging.debug("resp=%s",response_data)
-
-if 'error' in response_data:
-    raise Exception(response_data['error']['internal_message'])
-
-# get transfer spec returned by Faspex
+# extract the single transfer spec (we sent a single transfer request)
 t_spec = response_data["transfer_specs"][0]["transfer_spec"]
+logging.debug(t_spec)
 
 # add file list in transfer spec
 t_spec['paths'] = []
-for f in upload_files:
+for f in files_to_upload:
     t_spec['paths'].append({'source':f})
 
 # send files into package
