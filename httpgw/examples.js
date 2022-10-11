e56f8d601e38969d3c111552c42ae18b06c1d71e
Laurent Martin <laurent.martin.l@gmail.com>
2022-10-11 17:42:10 +0200

    add status on page

--- httpgw/examples.js
+++ httpgw/examples.js
@@ -1,77 +1,80 @@
 // sample client-side web application
 
 // files selected by user for upload
-var selected_upload_files=[];
+var selected_upload_files;
 // transfer monitor
-var monitorId = null;
+var monitorId;
 
 // helper function
 function readableBytes(bytes) {
     var i = Math.floor(Math.log(bytes) / Math.log(1024)),
-    sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
     return (bytes / Math.pow(1024, i)).toFixed(2) * 1 + ' ' + sizes[i];
 }
 // to be called when page is ready
 function httpgw_initialize() {
     // display configuration
     document.getElementById("server_address").innerHTML = config.node.url + " / " + config.node.user;
-    asperaHttpGateway.initHttpGateway(config.misc.httpgw_url+'/v1').then(response => {
+    asperaHttpGateway.initHttpGateway(config.misc.httpgw_url + '/v1').then(response => {
         console.log('HTTP Gateway SDK started', response);
         // register a transfer monitor
         monitorId = asperaHttpGateway.registerActivityCallback((result) => {
             result.transfers.forEach(transfer => {
-              console.log(`Event:
+                const status = `Event:
     - Id:         ${transfer.uuid},
     - Status:     ${transfer.status},
     - Percent:    ${(transfer.percentage * 100).toFixed(2)}%,
     - Data Sent:  ${readableBytes(transfer.bytes_written)},
-    - Data Total: ${readableBytes(transfer.bytes_expected)}`);
+    - Data Total: ${readableBytes(transfer.bytes_expected)}`;
+                console.log(status);
+                document.getElementById("status").innerHTML = status;
             });
         });
     }).catch(error => {
         console.error('HTTP Gateway SDK did not start', error);
-        alert("Prolem with HTTPGW:"+error.message);
+        alert("Prolem with HTTPGW:" + error.message);
     });
 }
 
 // call the server to get a transfer authorization (with token)
-function httpgw_get_ts(direction,files) {
+function httpgw_get_ts(direction, files) {
     return new Promise((resolve) => {
         // get transfer spec from server
-        fetch(window.location.href+'tspec',{
+        fetch(window.location.href + 'tspec', {
             method: 'POST',
-            headers: {"Content-Type":"application/json"},
-            body: JSON.stringify({"operation":direction,"files":files})
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ operation: direction, files: files })
         })
-        .then((response)=>{return response.json();})
-        .then((ts)=>{
-            ts.authentication="token";
-            //ts.download_name="project_files"
-            //ts.zip_required=true;
-            resolve(ts);
-        });
+            .then((response) => { return response.json(); })
+            .then((ts) => {
+                ts.authentication = "token";
+                //ts.download_name="project_files"
+                //ts.zip_required=true;
+                resolve(ts);
+            });
     });
 }
 
 // called by download button
 function httpgw_download() {
     console.log('Download asked');
-    httpgw_get_ts("download",[config.misc.server_file])
-    .then((transferSpec)=>{
-        console.log('>>transfer spec',transferSpec);
-        asperaHttpGateway.download(transferSpec).then(response => {
-            console.log('Download started', response);
-        }).catch(error => {
-            console.log('Download could not start', error); 
-            alert("Prolem with HTTPGW:"+error.message);
+    httpgw_get_ts("download", [config.misc.server_file])
+        .then((transferSpec) => {
+            console.log('>>transfer spec', transferSpec);
+            asperaHttpGateway.download(transferSpec).then(response => {
+                console.log('Download started', response);
+            }).catch(error => {
+                console.log('Download could not start', error);
+                alert("Prolem with HTTPGW:" + error.message);
+            });
         });
-    });
 }
 
 // called by file select button
 function httpgw_pick_files(formId) {
     asperaHttpGateway.getFilesForUpload((pick) => {
-        for (let file of pick.dataTransfer.files) {
+        selected_upload_files = [];
+        for (const file of pick.dataTransfer.files) {
             selected_upload_files.push(file.name);
         }
         console.log('Files picked', selected_upload_files);
@@ -80,18 +83,18 @@ function httpgw_pick_files(formId) {
 
 // called by upload button
 function httpgw_upload(formId) {
-    httpgw_get_ts("upload",selected_upload_files)
-    .then((transferSpec)=>{
-        asperaHttpGateway.upload(transferSpec, formId)
-        .then(response => {
-            // Indicates upload started; transfer status is shown in activity callbacks
-            console.log('Upload started', response);
-        }).catch(error => {
-            // Indicates upload could not start (this is a failure from the SDK or the Gateway Server, not from the transfer server)
-            console.log('Upload could not start', error);
-            alert("Prolem with HTTPGW:"+error.message);
+    httpgw_get_ts("upload", selected_upload_files)
+        .then((transferSpec) => {
+            asperaHttpGateway.upload(transferSpec, formId)
+                .then(response => {
+                    // Indicates upload started; transfer status is shown in activity callbacks
+                    console.log('Upload started', response);
+                }).catch(error => {
+                    // Indicates upload could not start (this is a failure from the SDK or the Gateway Server, not from the transfer server)
+                    console.log('Upload could not start', error);
+                    alert("Prolem with HTTPGW:" + error.message);
+                });
+            // reset
+            selected_upload_files = [];
         });
-        // reset
-        selected_upload_files=[];
-    });
 }
--- httpgw/server.js
+++ httpgw/server.js
@@ -10,13 +10,13 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 
 // command line arguments
-const configFile=process.argv[2];
+const configFile = process.argv[2];
 const port = Number(process.argv[3]);
 
 // read config file (node credentials ...) 
-const config=yaml.load(fs.readFileSync(configFile, 'utf8'));
+const config = yaml.load(fs.readFileSync(configFile, 'utf8'));
 // for demo, assume self signed cert
-const ignoreCertAgent = new https.Agent({rejectUnauthorized: false});
+const ignoreCertAgent = new https.Agent({ rejectUnauthorized: false });
 // web server
 const app = express();
 
@@ -30,30 +30,33 @@ app.use(bodyParser.json());
 // get transfer authorization by calling node API
 app.post('/tspec', async (req, res) => {
   // requested path for authorization
-  var request_paths=null;
-  // which files to transfer (for sample: 1 file only)
-  var source_paths=[{"source":req.body.files[0]}];
+  let request_paths = null;
+  const source_paths = [];
+  // which files to transfer
+  for (const file of req.body.files) {
+    source_paths.push({ source: file });
+  }
   if (req.body.operation === "upload") {
     // for sample: server chooses destination
-    request_paths=[{"destination":config.misc.server_folder}];
+    request_paths = [{ destination: config.misc.server_folder }];
   } else if (req.body.operation === "download") {
-    request_paths=source_paths;
+    request_paths = source_paths;
   } else {
     console.log(`ERROR: Unexpected request: ${req.body.operation}`);
     return;
   }
-  console.log("request:",req.body.operation);
+  console.log("paths:", request_paths);
   // call HSTS node API (with a single transfer request)
-  const ts_resp = await fetch(config.node.url+`/files/${req.body.operation}_setup`, {
+  const ts_resp = await fetch(config.node.url + `/files/${req.body.operation}_setup`, {
     method: 'POST',
-    headers: {"Authorization" : 'Basic '+Buffer.from(config.node.user+':'+config.node.pass).toString('base64')},
-    body: JSON.stringify({"transfer_requests":[{"transfer_request":{"paths":request_paths}}]}),
+    headers: { Authorization: 'Basic ' + Buffer.from(config.node.user + ':' + config.node.pass).toString('base64') },
+    body: JSON.stringify({ transfer_requests: [{ transfer_request: { paths: request_paths } }] }),
     agent: ignoreCertAgent
   });
   // wait for full answer
   const result = await ts_resp.json();
-  // one request was made, so one answer is received
-  var transfer_spec = result["transfer_specs"][0]["transfer_spec"];
+  // one request was made, so one answer is received (assume no error)
+  const transfer_spec = result.transfer_specs[0].transfer_spec;
   // set paths to transfer
   transfer_spec.paths = source_paths;
   console.log(transfer_spec);
