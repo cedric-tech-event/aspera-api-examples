16b3cb8029d392233227a0479f2e17e67152cfaf
Laurent Martin <laurent.martin.l@gmail.com>
2022-10-18 14:17:23 +0200

    add aspera token example

--- web/src/connect_sample.js
+++ web/src/connect_sample.js
@@ -2,9 +2,9 @@ function connect_initialize() {
   this.client = new AW4.Connect();
   this.client.initSession();
 }
-function connect_download_simple() {
+function connect_download_ssh_creds() {
   // replace ssh, as browser will not take ssh:
-  const server_url=new URL(config.server.url.replace(/^ssh:/g, "http://"));
+  const server_url=new URL(config.server.url.replace(/^ssh:/g, 'http://'));
   const transferSpec = {
     paths: [
       {
@@ -17,10 +17,10 @@ function connect_download_simple() {
     remote_password: config.server.pass,
     direction: 'receive'
   };
-  console.log("ts",transferSpec);
+  console.log('ts',transferSpec);
   this.client.startTransfer(transferSpec);
 }
-function connect_download_basic() {
+function connect_download_basic_token() {
   const node_url=new URL(config.nodeak.url);
   const transferSpec = {
     paths: [
@@ -35,6 +35,23 @@ function connect_download_basic() {
     authentication: 'token',
     direction: 'receive'
   };
-  console.log("ts",transferSpec);
+  console.log('ts',transferSpec);
   this.client.startTransfer(transferSpec);
+}
+function connect_download_aspera_token() {
+  const node_url=new URL(config.node.url);
+  fetch(node_url + '/files/download_setup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Basic ' + btoa(config.node.user + ':' + config.node.pass)},
+    body: JSON.stringify({'transfer_requests':[{'transfer_request':{'paths':[{'source':config.server.download_file}]}}]})})
+    .then((response) => { return response.json(); })
+    .then((result) => {
+      console.log('result',result);
+      const transferSpec = result.transfer_specs[0].transfer_spec;
+      transferSpec.authentication = 'token';
+      console.log('ts',transferSpec);
+      this.client.startTransfer(transferSpec);
+    });
 }
